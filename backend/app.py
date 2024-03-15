0359c3f74b997889a691c61b9bf740378ce0752a
Kevin SIMON <simonkvin@gmail.com>
2024-03-15 10:32:15 +0100

    Ajouter des endpoints pour les graphiques de prédiction de revenu et d'âge avec K-means et GMM

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from endpoints.pca import pca_predict
+from endpoints.kmeans_and_gmm import income_kmeans_predict, age_kmeans_predict, income_gmm_predict, age_gmm_predict
 
 app = FastAPI()
 
@@ -11,10 +12,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.get("/data_and_plot/")
-async def data_and_plot():
+@app.get("/plot_pca/")
+async def plot_pca():
     return pca_predict()
+
+@app.get("/plot_income_kmeans/")
+async def plot_income_kmeans():
+    return income_kmeans_predict()
+
+@app.get("/plot_age_kmeans/")
+async def plot_age_kmeans():
+    return age_kmeans_predict()
+
+@app.get("/plot_income_gmm/")
+async def plot_income_gmm():
+    return income_gmm_predict()
+
+@app.get("/plot_age_gmm/")
+async def plot_age_gmm():
+    return age_gmm_predict()
 
 if __name__ == '__main__':
     import uvicorn
